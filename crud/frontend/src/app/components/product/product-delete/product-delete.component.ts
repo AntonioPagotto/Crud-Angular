1332e683cadea29dfd691e2105e223a32e3b0265
AntonioPagotto <60796895+AntonioPagotto@users.noreply.github.com>
2020-08-10 09:57:51 -0300

    finish

--- crud/frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ crud/frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -1,14 +1,14 @@
-import { Product } from './../product.model';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ProductService } from './../product.service';
+import { Product } from './../product.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-product-update',
-  templateUrl: './product-update.component.html',
-  styleUrls: ['./product-update.component.css']
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
 })
-export class ProductUpdateComponent implements OnInit {
+export class ProductDeleteComponent implements OnInit {
 
   product: Product
 
@@ -18,17 +18,17 @@ export class ProductUpdateComponent implements OnInit {
     const id = +this.route.snapshot.paramMap.get('id');
     this.productService.readById(id).subscribe(product => {
       this.product = product
-    });
-  }
-
-  updateProduct(): void {
-    this.productService.update(this.product).subscribe(() => {
-      this.productService.showMessage('Produto atualizado com sucesso!');
-      this.router.navigate(['/products']);
     })
   }
 
-  cancel(): void {
-    this.router.navigate(['/products']);
+  deleteProduct(): void {
+    this.productService.delete(this.product.id).subscribe(() => {
+      this.productService.showMessage('Produto excluido com sucesso!')
+      this.router.navigate(['/products'])
+    })
   }
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+
 }
